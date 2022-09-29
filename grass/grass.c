2c52d329b99932d1965881df607edd4e95783db6
Yunhao Zhang <yz2327@cornell.edu>
2022-09-29 12:44:30 -0400

    Add timer handler example using mepc/mret

--- grass/grass.c
+++ grass/grass.c
@@ -1,18 +1,35 @@
 #include "egos.h"
 
-int quantum = 50000;
+int quantum = 5000;
 
-void handler()  __attribute__((interrupt));
-void handler() {
+void handler1()  __attribute__((interrupt));
+void handler1() {
     earth->tty_info("Got timer interrupt.");
     mtimecmp_set(mtime_get() + quantum);
+}
+
+void thread0() { while(1) {earth->tty_success("Something green");} }
+void thread1() { while(1) {earth->tty_critical("Something yellow");} }
+
+int next_thread = 0;
+void handler2()  __attribute__((interrupt));
+void handler2() {
+    /* Forget previous context
+     * and start a new thread calling thread0() or thread1() */
+    next_thread = 1 - next_thread;
+    asm("csrw mepc, %0" ::"r"((next_thread == 0)? thread0 : thread1));
+    /* Reset timer and enter the new thread */
+    mtimecmp_set(mtime_get() + quantum);
+    asm("li sp,0x80002000");
+    asm("mret");
 }
 
 int main() {
     earth->tty_success("A timer interrupt example.");
 
     /* Setup timer and register handler */
-    asm("csrw mtvec, %0" ::"r"(handler));
+    //asm("csrw mtvec, %0" ::"r"(handler1));
+    asm("csrw mtvec, %0" ::"r"(handler2));
     mtimecmp_set(mtime_get() + quantum);
     /* Enable timer interrupt */
     int mstatus, mie;
@@ -20,6 +37,6 @@ int main() {
     asm("csrw mstatus, %0" ::"r"(mstatus | 0x8));
     asm("csrr %0, mie" : "=r"(mie));
     asm("csrw mie, %0" ::"r"(mie | 0x80));
-    
+
     while(1);
 }
