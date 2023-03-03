4e367a86e20d1cf7b1d43ef0d2c24de6f8da5c82
Yunhao Zhang <yunhao@YunhaodeMacBook-Air.local>
2023-03-03 11:18:40 -0500

    Add timer interrupt example #1

--- grass/grass.c
+++ grass/grass.c
@@ -1,49 +1,27 @@
-/*
- * (C) 2022, Cornell University
- * All rights reserved.
- */
-
-/* Author: Yunhao Zhang
- * Description: grass layer initialization
- * Initialize timer and the process control block; 
- * Spawn the first kernel process, GPID_PROCESS (pid=1).
- */
-
 #include "egos.h"
-#include "process.h"
-#include "syscall.h"
 
-struct grass *grass = (void*)APPS_STACK_TOP;
+long long mtime_get();
+void mtimecmp_set(long long time);
 struct earth *earth = (void*)GRASS_STACK_TOP;
 
-static int sys_proc_read(int block_no, char* dst) {
-    return earth->disk_read(SYS_PROC_EXEC_START + block_no, 1, dst);
+int quantum = 5000;
+
+void handler() {
+    earth->tty_info("Got timer interrupt.");
+    mtimecmp_set(mtime_get() + quantum);
 }
 
 int main() {
-    CRITICAL("Enter the grass layer");
+    earth->tty_success("A timer interrupt example.");
 
-    /* Initialize the grass interface functions */
-    grass->proc_alloc = proc_alloc;
-    grass->proc_free = proc_free;
-    grass->proc_set_ready = proc_set_ready;
+    asm("csrw mtvec, %0" ::"r"(handler));
+    mtimecmp_set(mtime_get() + quantum);
 
-    grass->sys_exit = sys_exit;
-    grass->sys_send = sys_send;
-    grass->sys_recv = sys_recv;
+    int mstatus, mie;
+    asm("csrr %0, mstatus" : "=r"(mstatus));
+    asm("csrw mstatus, %0" ::"r"(mstatus | 0x8));
+    asm("csrr %0, mie" : "=r"(mie));
+    asm("csrw mie, %0" ::"r"(mie | 0x80));
     
-    /* Load and enter the first kernel process sys_proc */
-    INFO("Load kernel process #%d: sys_proc", GPID_PROCESS);
-    elf_load(GPID_PROCESS, sys_proc_read, 0, 0);
-    earth->mmu_switch(GPID_PROCESS);
-
-    proc_init();
-    timer_init();
-    timer_reset();
-    /* For page table translation, interrupt was enabled in earth layer */
-    if (earth->translation == SOFT_TLB) earth->intr_enable();
-
-    void (*sys_proc_entry)() = (void*)APPS_ENTRY;
-    asm("mv a0, %0" ::"r"(APPS_ARG));
-    sys_proc_entry();
+    while(1);
 }
